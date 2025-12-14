e3bbd6da2e9c3787f73afcf6ae80896147991458
exstacyagency <exstacyagency@gmail.com>
2025-12-14 21:08:22 +0000

    commit

--- prisma/migrations/20240512120000_add_job_idempotency/migration.sql
+++ prisma/migrations/20240512120000_add_job_idempotency/migration.sql
@@ -1,6 +1,13 @@
-ALTER TABLE "Job"
-ADD COLUMN "idempotencyKey" TEXT;
+DO $$
+BEGIN
+  IF to_regclass('public."Job"') IS NULL THEN
+    RAISE NOTICE 'Skipping add_job_idempotency migration because "Job" table does not exist yet.';
+    RETURN;
+  END IF;
 
-CREATE UNIQUE INDEX "Job_projectId_type_idempotencyKey_key"
-ON "Job"("projectId", "type", "idempotencyKey")
-WHERE "idempotencyKey" IS NOT NULL;
+  ALTER TABLE "Job" ADD COLUMN IF NOT EXISTS "idempotencyKey" TEXT;
+
+  CREATE UNIQUE INDEX IF NOT EXISTS "Job_projectId_type_idempotencyKey_key"
+  ON "Job"("projectId", "type", "idempotencyKey")
+  WHERE "idempotencyKey" IS NOT NULL;
+END $$;
