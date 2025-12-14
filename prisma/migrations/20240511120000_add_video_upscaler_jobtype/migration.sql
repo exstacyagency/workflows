5494f4202de708eac06fa66160039cafd4f0194c
exstacyagency <exstacyagency@gmail.com>
2025-12-14 20:38:08 +0000

    commit

--- prisma/migrations/20240511120000_add_video_upscaler_jobtype/migration.sql
+++ prisma/migrations/20240511120000_add_video_upscaler_jobtype/migration.sql
@@ -1,2 +1,23 @@
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'JobType') THEN
+    -- Create enum with all current JobType values from prisma/schema.prisma
+    CREATE TYPE "JobType" AS ENUM (
+      'CUSTOMER_RESEARCH',
+      'AD_PERFORMANCE',
+      'AD_TRANSCRIPTS',
+      'PATTERN_ANALYSIS',
+      'SCRIPT_GENERATION',
+      'STORYBOARD_GENERATION',
+      'CUSTOMER_ANALYSIS',
+      'CHARACTER_GENERATION',
+      'VIDEO_IMAGE_GENERATION',
+      'VIDEO_PROMPT_GENERATION',
+      'VIDEO_REVIEW',
+      'VIDEO_UPSCALER'
+    );
+  END IF;
+END $$;
+
 -- Add VIDEO_UPSCALER enum value for JobType
 ALTER TYPE "JobType" ADD VALUE IF NOT EXISTS 'VIDEO_UPSCALER';
