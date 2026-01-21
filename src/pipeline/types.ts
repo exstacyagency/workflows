d06c7c97791661fc44d33d01c23b92beb2822739
YOUR_GITHUB_USERNAME <your@email.com>
2026-01-21 19:11:15 +0000

    Add Pattern Brain stub and wire into pipeline

--- src/pipeline/types.ts
+++ src/pipeline/types.ts
@@ -1,8 +1,10 @@
 import { ResearchArtifacts } from "./contracts/research";
+import { PatternBrainArtifacts } from "./patternBrain/types";
 
 export type PipelineArtifacts = {
   research?: ResearchArtifacts;
   patterns?: unknown;
+  patternBrain?: PatternBrainArtifacts;
   character?: unknown;
   script?: unknown;
   videoPrompts?: unknown;
